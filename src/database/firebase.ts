b92c2d43c97ff5e76c203a6f4d5d616f4b1debc0
RomanOnyshko <asasin09867@gmail.com>
2024-12-10 11:30:21 +0200

    firt commit

---
+++ src/database/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAEf7nqoZiBqVoY0gBdfHJeGWRT9z018pc",
+  authDomain: "todolistreact-ca3cc.firebaseapp.com",
+  projectId: "todolistreact-ca3cc",
+  storageBucket: "todolistreact-ca3cc.firebasestorage.app",
+  messagingSenderId: "294465113578",
+  appId: "1:294465113578:web:708fa275e61263e6ff7693",
+  measurementId: "G-4RTZMWREGP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
+
